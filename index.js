4b4b3d73d99a60c9617859f006e25a3fab592228
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2020-09-21 15:18:35 -0400

    Ajout d'une base de données json

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 console.log('Bienvenue dans mon TP1');
+
+const fs = require('fs');
+
+const monObjet = {
+  a: 34,
+  b: 56,
+  salut: 'bonjour mon ami!'
+};
+
+fs.writeFileSync('bdd.json', JSON.stringify(monObjet));
+
+const contenuDeMonObjet = JSON.parse(fs.readFileSync('bdd.json'));
+
+console.log('mon objet:', contenuDeMonObjet);
+
+/*
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: 'number',
+      name: 'age',
+      message: 'Quel est votre âge?'
+    },
+    {
+      type: 'input',
+      name: 'nom',
+      message: 'Quel est votre nom?'
+    }
+  ])
+  .then(answers => {
+    console.log(`Bonjour ${answers.nom}, vous avez ${answers.age} ans. Bienvenue!`);
+    // Use user feedback for... whatever!!
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
+  */
