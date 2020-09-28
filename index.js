0fcbd1f34f3789fa0ad9f4a98c80a5d24dbecadf
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2020-09-28 15:03:35 -0400

    ajout de fonctionnalités des Promesses

--- index.js
+++ index.js
@@ -1,4 +1,77 @@
-console.log('Bienvenue dans mon TP1');
+
+const fetch = require('node-fetch');
+// url bixi: https://api-core.bixi.com/gbfs/en/station_information.json
+
+const urlBixi = 'https://api-core.bixi.com/gbfs/en/station_information.json';
+
+/*fetch(urlBixi).then(function(response) {
+  return response.json().then(function(jsonResponse) {
+    console.log(jsonResponse.data.stations[0]);
+  });
+});*/
+
+const maFonctionAsynchrone = async function() {
+  const bixiResponse = await fetch(urlBixi);
+  const jsonResponse = await bixiResponse.json();
+  return jsonResponse.data.stations[0];
+};
+
+const maPromesse = new Promise(function(resolve, reject) {
+  const a = 23 + 45;
+  resolve(a + 55);
+});
+
+maPromesse.then(function(somme) {
+  console.log('ma somme est' + somme);
+});
+
+maFonctionAsynchrone().then(function(result) {
+  console.log(result);
+});
+
+console.log('Bonjour');
+
+
+/*const monArray = [1,2,4,6,8,-3,45,-23];
+
+console.log(monArray.sort(function(a, b) {
+  return b - a;
+}));
+
+const mesBus = [
+  {
+    capacite: 25,
+    immatriculation: 'A12345'
+  },
+  {
+    capacite: 20,
+    immatriculation: 'A01234'
+  },
+  {
+    capacite: 32,
+    immatriculation: 'A23456'
+  },
+  {
+    capacite: 12,
+    immatriculation: 'A67890'
+  }
+];
+
+/*console.log(mesBus.sort(function(bus1, bus2) {
+  return bus1.capacite - bus2.capacite;
+}));*/
+
+/*const plaque = 'A23456';
+
+console.log(mesBus.filter(function(bus) {
+  return bus.immatriculation === plaque;
+}));*/
+
+//console.log(mesBus);
+
+
+
+/*console.log('Bienvenue dans mon TP1');
 
 const fs = require('fs');
 
@@ -40,3 +113,5 @@ inquirer
     }
   });
   
+
+  */
